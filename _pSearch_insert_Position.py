685315bbbfb0c8d60867249f8114200adc5e415f
Sonikac28 <sonikacbsc24@rvu.edu.in>
2026-01-30 10:52:19 +0530

    Update _pSearch_insert_Position.py

--- _pSearch_insert_Position.py
+++ _pSearch_insert_Position.py
@@ -1,7 +1,1 @@
-class Solution(object):
-  def containsDuplicate(slef, nums):
-    nums= sorted(nums)
-    for i in range(len(nums)-1_:
-      if nums[i] == nums[i+1]:
-        return True
-    return False
+
